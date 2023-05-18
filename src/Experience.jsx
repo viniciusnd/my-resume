81f7e3871cb7ab79a630290fadd059908dc1ad66
viniciusnd <vinicius.santosnd@gmail.com>
2023-05-18 09:34:31 -0300

    docs

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -7,11 +7,17 @@ export default function Experience() {
       </div>
       <header className="text-white text-center font-playfair mt-5 text-2xl md:text-3xl uppercase"> Experiences </header>
       <div className="text-white text-left text-sm md:text-base flex flex-col ml-5 mt-5 md:text-center">
+        <p4 className="flex flex-row mt-5 font-bold">Segula Technologies:</p4>
+        <ul className="ml-5 text-sm md:text-base mt-3 flex flex-col mb-3 md:flex-col md:text-left mr-5 md:mr-3">
+          <li>- Role: Product Specialist || Software Engineer | May 2023 - Present</li>
+          <li>- Activities: Functional Analysis | Interpretation of Software Requirements| Developing Algorithms and Models | Simulation, Validation, Integration, Corrections, Evolutions and Versioning of Software</li>
+          <li>- Tech: MIL, SIL, Matlab, Simulink, Model Based Design, CAN, LIN, C, C++, SCRUM, Engines Systems</li>
+        </ul>
         <p4 className="flex flex-row mt-5 font-bold">MSX International | Zeentech:</p4>
         <ul className="ml-5 text-sm md:text-base mt-3 flex flex-col mb-3 md:flex-col md:text-left">
-          <li>- Role: Software Engineer | Aug 2022 - Present</li>
+          <li>- Role: Software Engineer | Aug 2022 - May 2023</li>
           <li>- Activities: Software test and validation | Process Development | Creation and Management of Reports</li>
-          <li>- Tech: Unit Test, SIL, Matlab, Simulink, CAN, C, C++, Python, SCRUM, Code Coverage(CTC++), GIT, JIRA</li>
+          <li>- Tech: Unit Test, SIL, Matlab, Simulink, CAN, Infotainment Systems, C, C++, Python, SCRUM, Code Coverage, GIT, JIRA</li>
         </ul>
         <p4 className="flex flex-row mt-5 font-bold">Fuse IoT:</p4>
         <ul className="ml-5 text-sm md:text-base mt-3 flex flex-col md:flex-col md:text-left">
@@ -20,7 +26,7 @@ export default function Experience() {
           <li>- Tech: JavaScript, NodeRed, C++, Docker, Grafana, GIT, Arduino, Esp32</li>
         </ul>
         <p4 className="flex flex-row mt-5 font-bold">Other Companies:</p4>
-        <ul className="ml-5 text-sm md:text-base mt-3 flex flex-col md:flex-col md:text-left">
+        <ul className="ml-5 text-sm md:text-base mt-3 flex flex-col md:flex-col md:text-left mb-5">
           <li>- S3 Aeronautical Maintanence | Electronic & Quality Technician | Apr 2021 - Apr 2022 </li>
           <li>- Delta Greentech | Engineering Intern | Dec 2019 - Aug 2020 </li>
           <li>- Falcão Bauer | Electronics Internship | Apr 2017 - Jul 2022 </li>
